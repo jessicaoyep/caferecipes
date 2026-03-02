378da8463fd86463d3bb5e0038d38888786c48cf
jessicaoyep <jessicaoyep@gmail.com>
2026-03-01 17:13:12 -0800

    Add files via upload
    commit all

---
+++ cafedrink/README.md
@@ -0,0 +1,27 @@
+# cafedrinks
+<<<<<<< HEAD
+=======
+
+to see what files you changed:
+
+git status
+
+-----
+
+add the updated files:
+
+git add .
+
+-----
+
+replace message with what you've changed
+
+git commit -m "message"
+
+-----
+
+push your changes:
+
+git push
+
+>>>>>>> 67b9899e50343bd4fc89f7b2d8fd3618801102e8
---
+++ cafedrink/matcha.js
@@ -0,0 +1,15 @@
+
+/* JavaScript for Matcha Recipes page*/
+var popup = document.getElementById("popup");
+
+function openPopup() {
+    popup.classList.add("open-popup");
+}
+
+function closePopup() {
+    popup.classList.remove("open-popup");
+}
+
+
+/* article recipe card info */
+const drinkName = "Stagger Double Matcha Latte";
